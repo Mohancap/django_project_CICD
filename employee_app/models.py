9d95304e726d6efab9e0375bff9f906584d1cd19
Mohancap <muralimohanmp03@gmail.com>
2022-05-06 22:00:16 +0530

    modified in model file, html file

--- employee_app/models.py
+++ employee_app/models.py
@@ -18,11 +18,11 @@ class Employee_details(models.Model):
 
 class Employee_personal_details(models.Model):
     employee_name = models.CharField(max_length=100)
-    employee_id = models.IntegerField()
-    contact_num = models.IntegerField()
+    employee_id = models.CharField(max_length=100)
+    contact_num = models.CharField(max_length=100)
     company_location = models.CharField(max_length=100)
-    emp_salary = models.IntegerField()
-    emp_age = models.IntegerField()
+    emp_salary = models.CharField(max_length=100)
+    emp_age = models.CharField(max_length=100)
     emp_permanant_address = models.TextField()
     emp_email_address = models.CharField(max_length=200)
     emp_current_address = models.TextField()
