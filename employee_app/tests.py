779f9400a2c3fb9cd4e3f6d4df4bf15d2eacb992
Mohancap <muralimohanmp03@gmail.com>
2022-05-07 01:39:23 +0530

    modified files

--- employee_app/tests.py
+++ employee_app/tests.py
@@ -1,3 +1,19 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Employee_details
+
+
+class EmployeeTests(TestCase):
+    def test_page_is_created_successfully(self):
+        employee_information = Employee_details(
+            title = "Employee details",
+            name = "Murali",
+            age = 28,
+            salary = 100000,
+            location = "Bangalore",
+            about_self = "Hi, This is Murali Mohan, Working at Capgemini in Bangalore",
+            created_at = "time"
+        )
+        employee_information.save()
