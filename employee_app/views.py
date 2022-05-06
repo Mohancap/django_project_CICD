619e4f1fcfeb46d84ee11b9eb8ccc3d2be984af6
Mohancap <muralimohanmp03@gmail.com>
2022-05-06 23:25:04 +0530

    modified files

--- employee_app/views.py
+++ employee_app/views.py
@@ -7,10 +7,12 @@ from .models import Employee_details, Employee_personal_details
 
 def base(request):
     post = Employee_details.objects.all()
-    return render(request, "base.html", {"post":post})
+    return render(request, "base.html", {"post" : post})
+
 
 def first(request):
-    return render(request, "first.html", {"name":"Murali"})
+    return render(request, "first.html", {"name" : "Murali"})
+
 
 def employee_contact_infor(request):
     if request.method == 'POST':
@@ -25,12 +27,13 @@ def employee_contact_infor(request):
         emp_current_address = request.POST['current_address']
         created_date = request.POST.get('date')
         employee_details = Employee_personal_details(employee_name = employee_name, employee_id = employee_id, contact_num = contact_num, 
-                                                    company_location = company_location, emp_salary = emp_salary, emp_age = emp_age, 
-                                                    emp_permanant_address = emp_permanant_address, emp_email_address = emp_email_address, 
-                                                    emp_current_address = emp_current_address, created_date = created_date)
+        company_location = company_location, emp_salary = emp_salary, emp_age = emp_age, 
+        emp_permanant_address = emp_permanant_address, emp_email_address = emp_email_address, 
+        emp_current_address = emp_current_address, created_date = created_date)
         employee_details.save()
     return render(request,'form_to_database.html')
 
+
 def employee_info(request):
     post = Employee_personal_details.objects.all()
-    return render(request, "base.html", {"post":post})
+    return render(request, "base.html", {"post" : post})
