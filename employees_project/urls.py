92215d3ce080d32c55f15f8054968f0c3a377992
Mohancap <muralimohanmp03@gmail.com>
2022-05-06 22:59:15 +0530

    modified files

--- employees_project/urls.py
+++ employees_project/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from .views import demo
 from django.conf import settings
 from django.conf.urls.static import static
-from employee_app.views import first, base, employee_contact_infor, employee_info
+from employee_app.views import first,base,employee_contact_infor,employee_info
 
 
 urlpatterns = [
