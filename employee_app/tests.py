514e757f9385570415daca5ba560e67652b51ff2
Mohancap <muralimohanmp03@gmail.com>
2022-05-09 13:06:03 +0530

    modified code

--- employee_app/tests.py
+++ employee_app/tests.py
@@ -8,17 +8,9 @@ from .models import Employee_information
 
 class EmployeeTests(TestCase):
     def test_page_is_created_successfully(self):
-        self.employee_app = Employee_information.objects.create(
-            title = "Bangalore",
-            name = "Bangalore",
-            # age = 28,
-            # salary = 100000,
-            location = "Bangalore",
-            # about_self = "Hi, This is Murali Mohan, Working at Capgemini in Bangalore"
-            # created_at = datetime.now()
-        )
+        self.employee_app = Employee_information.objects.create(title = "Bangalore", name = "Bangalore", location = "Bangalore")
 
 def employee_information(self):
     e = self.employee_app
     self.assertTrue(isinstance(e, Employee_information))
-    self.assertEqual(str(e), 'Bangalore')
+    self.assertEqual(str(e), 'Bangalor')
