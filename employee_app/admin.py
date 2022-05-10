2e2cbe8604b7a1e72154b8f9e944ca414e632208
Mohancap <105060109+Mohancap@users.noreply.github.com>
2022-05-10 10:45:24 +0530

    Update admin.py

--- employee_app/admin.py
+++ employee_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Employee_details, Employee_personal_details
+from .models import Employee_details, Employee_personal_details, Employee_information
 
 # Register your models here.
 
 admin.site.register(Employee_details)
 admin.site.register(Employee_personal_details)
+admin.site.register(Employee_information)
